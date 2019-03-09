82badefb17044e1ab6000a238a66b7dd4b4403a3
hoang <hoangpham2395@gmail.com>
2019-03-09 16:56:16 +0700

    [Backend] Update navbar

--- resources/views/layouts/backend/structure/navbar.blade.php
+++ resources/views/layouts/backend/structure/navbar.blade.php
@@ -16,7 +16,7 @@
                 <li class="dropdown user user-menu">
                     <a href="#" class="dropdown-toggle" data-toggle="dropdown">
                         <img src="{{getAvatarDefault()}}" class="user-image" alt="User Image">
-                        <span class="hidden-xs">Admin</span>
+                        <span class="hidden-xs">{{getCurrentAdmin()->username}}</span>
                     </a>
                     <ul class="dropdown-menu">
                         <!-- User image -->
