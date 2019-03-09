b8229fb88138ef5014f033256f7b9946e0763f62
hoang <hoangpham2395@gmail.com>
2019-03-10 01:39:40 +0700

    [Backend] Update seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,5 +13,7 @@ class DatabaseSeeder extends Seeder
     {
         $this->call(AdminTableSeeder::class);
         $this->call(UsersTableSeeder::class);
+        $this->call(CitiesTableSeeder::class);
+        $this->call(DistrictsTableSeeder::class);
     }
 }
