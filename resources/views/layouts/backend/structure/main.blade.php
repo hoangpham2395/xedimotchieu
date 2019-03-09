485a3c54bacd6d251842126e4eff29c2cc33abbd
hoang <hoangpham2395@gmail.com>
2019-03-09 18:22:46 +0700

    [Backend] Add favicon

--- resources/views/layouts/backend/structure/main.blade.php
+++ resources/views/layouts/backend/structure/main.blade.php
@@ -5,6 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>{{getConstant('BACKEND_NAME')}} | Administrator</title>
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ url('favicon.png') }}">
     @include('layouts.backend.load.css')
 </head>
 <body class="hold-transition skin-red sidebar-mini">
