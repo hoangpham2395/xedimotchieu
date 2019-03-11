c367fcc8e4f6fec5a73d70e44c26bafbd5a011ff
HoangPH <hoangph.paraline@gmail.com>
2019-03-11 14:33:00 +0700

    [Backend] Update view login

--- resources/views/backend/auth/login.blade.php
+++ resources/views/backend/auth/login.blade.php
@@ -21,24 +21,24 @@
 
     {!! Form::open(['route' => 'admin.login', 'method' => 'POST']) !!}
       <div class="form-group has-feedback">
-        {!! Form::email('email', null, ['class' => 'form-control', 'placeholder' => 'Email']) !!}
+        {!! Form::email('email', null, ['class' => 'form-control', 'placeholder' => transm('admin.email')]) !!}
         <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
       </div>
       <div class="form-group has-feedback">
-        {!! Form::password('password', ['class' => 'form-control', 'placeholder' => 'Password']) !!}
+        {!! Form::password('password', ['class' => 'form-control', 'placeholder' => transm('admin.password')]) !!}
         <span class="glyphicon glyphicon-lock form-control-feedback"></span>
       </div>
       <div class="row">
         <div class="col-xs-8">
           <div class="checkbox icheck">
             <label>
-              <input type="checkbox" name="remember_me"> Remember Me
+              <input type="checkbox" name="remember_me"> {{transa('remember_me')}}
             </label>
           </div>
         </div>
         <!-- /.col -->
         <div class="col-xs-4">
-          {!! Form::submit('Sign In', ['class' => 'btn btn-primary btn-block btn-flat']) !!}
+          {!! Form::submit(transa('login'), ['class' => 'btn btn-primary btn-block btn-flat']) !!}
         </div>
         <!-- /.col -->
       </div>
