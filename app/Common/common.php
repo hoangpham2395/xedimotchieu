07f707022141812e61720f06cfa053a32575c994
HoangPH <hoangph.paraline@gmail.com>
2019-03-11 18:03:22 +0700

    [Backend] Update function common

--- app/Common/common.php
+++ app/Common/common.php
@@ -1,104 +1,215 @@
 <?php
-
-function getConstant($key, $default = null)
-{
-	return config('constant.' . $key, $default);
+if (! function_exists('getConstant')) {
+    /**
+     * @param $key
+     * @param null $default
+     * @return \Illuminate\Config\Repository|mixed
+     */
+    function getConstant($key, $default = null)
+    {
+        return config('constant.' . $key, $default);
+    }
 }
 
-function getMessage($key, $params = []) 
-{
-	return trans('messages.' . $key, $params);
+if (! function_exists('getMessage')) {
+    /**
+     * @param $key
+     * @param array $params
+     * @return array|\Illuminate\Contracts\Translation\Translator|null|string
+     */
+    function getMessage($key, $params = [])
+    {
+        return trans('messages.' . $key, $params);
+    }
 }
 
-function getConfig($key, $default = null) 
-{
-	return config('config.' . $key, $default);
+if (! function_exists('getConfig')) {
+    /**
+     * @param $key
+     * @param null $default
+     * @return \Illuminate\Config\Repository|mixed
+     */
+    function getConfig($key, $default = null)
+    {
+        return config('config.' . $key, $default);
+    }
 }
 
-function backendGuard() 
-{
-	return Auth::guard('web');
+if (! function_exists('backendGuard')) {
+    /**
+     * @return mixed
+     */
+    function backendGuard()
+    {
+        return Auth::guard('web');
+    }
 }
 
-function frontendGuard() 
-{
-	return Auth::guard('frontend');
+if (! function_exists('frontendGuard')) {
+    /**
+     * @return mixed
+     */
+    function frontendGuard()
+    {
+        return Auth::guard('frontend');
+    }
 }
 
-function getCurrentAdmin() 
-{
-	return backendGuard()->user();
+if (! function_exists('getCurrentAdmin')) {
+    /**
+     * @return mixed
+     */
+    function getCurrentAdmin()
+    {
+        return backendGuard()->user();
+    }
 }
 
-function getCurrentAdminId() 
-{
-	return backendGuard()->user()->id;
+if (! function_exists('getCurrentAdminId')) {
+    /**
+     * @return mixed
+     */
+    function getCurrentAdminId()
+    {
+        return backendGuard()->user()->id;
+    }
 }
 
-function getAvatarDefault()
-{
-	return asset(getConfig('avatar_default'));
+if (! function_exists('getAvatarDefault')) {
+    /**
+     * @return string
+     */
+    function getAvatarDefault()
+    {
+        return asset(getConfig('avatar_default'));
+    }
 }
 
-function getNoImage() 
-{
-	return asset(getConfig('no_image'));
+if (! function_exists('getNoImage')) {
+    /**
+     * @return string
+     */
+    function getNoImage()
+    {
+        return asset(getConfig('no_image'));
+    }
 }
 
-function getTmpUrl() 
-{
-	return asset(getConfig('url_tmp'));
+if (! function_exists('getTmpUrl')) {
+    /**
+     * @return string
+     */
+    function getTmpUrl()
+    {
+        return asset(getConfig('url_tmp'));
+    }
 }
 
-function getMediaUrl($alias = null) 
-{
-	$url = asset(getConfig('url_media'));
-	if (!$alias) {
-		return $url;
-	}
-	return $url . '/' . $alias;
+if (! function_exists('getMediaUrl')) {
+    /**
+     * @param null $alias
+     * @return string
+     */
+    function getMediaUrl($alias = null)
+    {
+        $url = asset(getConfig('url_media'));
+        if (!$alias) {
+            return $url;
+        }
+        return $url . '/' . $alias;
+    }
 }
 
-function getCurrentUser() 
-{
-	return Auth::user();
+if (! function_exists('getCurrentUser')) {
+    /**
+     * @return mixed
+     */
+    function getCurrentUser()
+    {
+        return Auth::user();
+    }
 }
 
-function transm($key)
-{
-	return trans('model.' . $key);
+if (! function_exists('transm')) {
+    /**
+     * @param $key
+     * @return array|\Illuminate\Contracts\Translation\Translator|null|string
+     */
+    function transm($key)
+    {
+        return trans('model.' . $key);
+    }
 }
 
-function transb($key) 
-{
-	return trans('breadcrumb.' . $key);
+if (! function_exists('transb')) {
+    /**
+     * @param $key
+     * @return array|\Illuminate\Contracts\Translation\Translator|null|string
+     */
+    function transb($key)
+    {
+        return trans('breadcrumb.' . $key);
+    }
 }
 
-function logError($message, array $context = [])
-{
-	// try {
-	// 	ChannelLog::error('error', $message, $context);
-	// } catch (\Exception $e) {
+if (! function_exists('logError')) {
+    /**
+     * @param $message
+     * @param array $context
+     */
+    function logError($message, array $context = [])
+    {
+        // try {
+        // 	ChannelLog::error('error', $message, $context);
+        // } catch (\Exception $e) {
 
-	// }
+        // }
+    }
 }
 
-function transa($key) 
-{
-	return trans('action.' . $key);
+if (! function_exists('transa')) {
+    /**
+     * @param $key
+     * @return array|\Illuminate\Contracts\Translation\Translator|null|string
+     */
+    function transa($key)
+    {
+        return trans('action.' . $key);
+    }
 }
 
-function getBackendAlias() 
-{
-	return env('BACKEND_ALIAS', 'management');
+if (! function_exists('getBackendAlias')) {
+    /**
+     * @return mixed
+     */
+    function getBackendAlias()
+    {
+        return env('BACKEND_ALIAS', 'management');
+    }
 }
 
-function rjust($string, $totalLength, $fillChar = ' ')
-{
-    return str_pad($string, $totalLength, $fillChar, STR_PAD_RIGHT);
+if (! function_exists('rjust')) {
+    /**
+     * @param $string
+     * @param $totalLength
+     * @param string $fillChar
+     * @return string
+     */
+    function rjust($string, $totalLength, $fillChar = ' ')
+    {
+        return str_pad($string, $totalLength, $fillChar, STR_PAD_RIGHT);
+    }
 }
 
-function ljust($string, $totalLength, $fillChar = ' ')
-{
-    return str_pad($string, $totalLength, $fillChar, STR_PAD_LEFT);
+if (! function_exists('ljust')) {
+    /**
+     * @param $string
+     * @param $totalLength
+     * @param string $fillChar
+     * @return string
+     */
+    function ljust($string, $totalLength, $fillChar = ' ')
+    {
+        return str_pad($string, $totalLength, $fillChar, STR_PAD_LEFT);
+    }
 }
