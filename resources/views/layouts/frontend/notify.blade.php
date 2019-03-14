1a8517dbd462d7c6e7bffc7f3551224b7b72425e
hoang <hoangpham2395@gmail.com>
2019-03-14 13:51:44 +0700

    [Frontend] Update layout notify

--- resources/views/layouts/frontend/notify.blade.php
+++ resources/views/layouts/frontend/notify.blade.php
@@ -1,8 +1,8 @@
 <div class="content content-custom">
 	<div class="row">
 		<div class="col-md-12">
-			@include('layouts.backend.success')
-			@include('layouts.backend.errors')
+			@include('layouts.frontend.success')
+			@include('layouts.frontend.errors')
 		</div>
 	</div>
 </div>
