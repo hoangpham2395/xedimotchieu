499ace73e04711e21ca725d123494304c331f465
hoang <hoangpham2395@gmail.com>
2019-03-20 02:09:48 +0700

    [Frontend] Update modal rates

--- resources/views/frontend/rates/_modal_check_login.blade.php
+++ resources/views/frontend/rates/_modal_check_login.blade.php
@@ -13,7 +13,7 @@
 				<span>{{getMessage('rates_require_login')}}</span>
 			</div>
 			<div class="modal-footer">
-				<button type="button" data-url="{{route('frontend.login', ['return_url' => route('home.community.detail', ['id' => $postId])])}}" class="btn btn-primary" data-dismiss="modal" onclick="RatesController.redirectLogin(this);">{{transa('login')}}</a>
+				<button type="button" data-url="{{route('frontend.login', ['return_url' => route('home.community.detail', ['id' => $postId])])}}" class="btn btn-primary" data-dismiss="modal" onclick="RatesController.redirectLogin(this);">{{transa('login')}}</button>
 				<button type="button" class="btn btn-danger" data-dismiss="modal">{{transa('cancel')}}</button>
 			</div>
 		</div>
