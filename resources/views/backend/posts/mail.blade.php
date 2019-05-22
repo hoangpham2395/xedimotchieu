8df0e72fdab25797797fdfe6d383ce01df0b08aa
HoangPH <hoangph.paraline@gmail.com>
2019-05-22 14:06:35 +0700

    [Backend] Add view mail delete post

---
+++ resources/views/backend/posts/mail.blade.php
@@ -0,0 +1,3 @@
+Xin chào <b>{{$name}}</b>!<br>
+<b>Cộng đồng xe một chiều</b> xin thông báo cho bạn rằng <b>bài đăng của bạn đã xóa</b> sau khi chúng tôi điều tra và xem xét do nhận được nhiều phản hồi không tốt!<br>
+Nội dung của bài đăng: {{$note}}
