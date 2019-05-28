4683e0075a47fa1c8051fb54ec816c585f7c9348
hoang <hoangpham2395@gmail.com>
2019-05-29 02:18:19 +0700

    [Frontend] Update detail and suggest post

--- resources/views/frontend/home/_suggest.blade.php
+++ resources/views/frontend/home/_suggest.blade.php
@@ -1,7 +1,7 @@
 @if (!empty($suggests->count()))	
 	@foreach ($suggests as $suggest)
 		<div class="w3-container w3-card w3-white w3-round w3-margin-custom padding-bottom"><br>
-			<img src="{{$entity->user->getUrlImage()}}" alt="Avatar" class="w3-left w3-circle w3-margin-right" style="width:60px; height: 60px;">
+			<img src="{{$suggest->user->getUrlImage()}}" alt="Avatar" class="w3-left w3-circle w3-margin-right" style="width:60px; height: 60px;">
 			<h4>{{$suggest->getPlace()}}</h4>
 			<h6>{{$suggest->user->name}} ({{$suggest->phone}}) | <a href="{{$suggest->getUrlChat()}}">Chat</a> <a href="{{route('home.community.detail', $suggest->id)}}" style="float: right;">Chi tiết</a></h6>
 		</div>
